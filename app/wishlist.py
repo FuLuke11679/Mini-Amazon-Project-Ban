a85f9daa4ae20a59d091b0896c0cef986cd2cdcf
Jun Yang <junyang@gmail.com>
2023-09-20 18:19:19 -0400

    completing wishlist add

--- app/wishlist.py
+++ app/wishlist.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, url_for, redirect
 from flask_login import current_user
 import datetime
 
@@ -14,5 +14,14 @@ def wishlist():
         items = WishlistItem.get_all_by_uid_since(
             current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
         return jsonify([item.__dict__ for item in items])
+    else:
+        return jsonify({}), 404
+
+
+@bp.route('/wishlist/add/<int:product_id>', methods=['POST'])
+def wishlist_add(product_id):
+    if current_user.is_authenticated:
+        WishlistItem.add(current_user.id, product_id, datetime.datetime.now())
+        return redirect(url_for('wishlist.wishlist'))
     else:
         return jsonify({}), 404
