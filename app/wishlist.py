a5d2d05b253e59ab928b0e2f94ec34c5822044ab
Jason Zhao <jwz10@duke.edu>
2023-10-03 19:17:37 -0400

    finished wishlist add

--- app/wishlist.py
+++ app/wishlist.py
@@ -4,9 +4,12 @@ import datetime
 
 from .models.product import Product
 from .models.wishlist import WishItem
+from flask import current_app as app
 
 from flask import Blueprint
 bp = Blueprint('wishlist', __name__)
+
+from flask import redirect, url_for
 
 
 @bp.route('/wishlist')
@@ -20,3 +23,14 @@ def index():
         return jsonfiy({}), 404
     # render the page by adding information to the index.html file
     return jsonify([item.__dict__ for item in wishlists])
+
+@bp.route('/wishlist/add/<int:product_id>', methods=['POST'])
+def wishlist_add(product_id):
+        rows = app.db.execute("""
+INSERT INTO Wishes(uid, pid, time_added)
+VALUES(:uid, :pid, :time_added)
+""",
+                                  uid=current_user.id,
+                                  pid=product_id,
+                                  time_added=datetime.datetime.now())
+        return redirect(url_for('wishlist.index'))
