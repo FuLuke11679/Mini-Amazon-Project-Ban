4d6e9bd006b92b046e76e3532455380267a6bd7f
lmf59 <luke.fu@duke.edu>
2023-10-05 18:09:58 -0400

    Added cart_search

--- app/cart.py
+++ app/cart.py
@@ -4,6 +4,7 @@ from flask_login import current_user
 import datetime
 from flask import redirect, url_for
 from flask import flash
+from flask import request
 
 from humanize import naturaltime
 from .models.product import Product
@@ -25,25 +26,30 @@ def cart():
             current_user.id)
     else:
         return jsonify({}), 404
-    # render the page by adding information to the index.html file
-    return render_template('cart.html',
-                      cartlist=cartlist,
-                      humanize_time=humanize_time)
+     #render the page by adding information to the index.html file
 
+    
+@bp.route('/cart_search', methods = ['GET', 'POST'])
+def cart_search():
+    if request.method == 'POST':
+        user_id = request.form['user_id']
+        cartlist = CartItem.get_all(user_id)
+        return render_template('cart.html',
+                      cartlist=cartlist,
+                      humanize_time=humanize_time,
+                      user_id = user_id) 
+    else:
+        return render_template('cart.html')
 
 @bp.route('/cart/add/<int:product_id>', methods=['POST'])
 def cart_add(product_id):
     if current_user.is_authenticated:
         CartItem.add(current_user.id, product_id, datetime.datetime.now())
         flash("Item added to cart successfully", "success")
-        cartlist = CartItem.get(current_user.id)
+        cartlist = CartItem.get_all(current_user.id)
         return redirect(url_for('cart.cart'))
     else:
         return jsonify({}), 404
 
 
-@bp.route('/cart/getuser/<int:user_id>', methods=['POST'])
-def cart_get(uid):
-    cartlist = CartItem.get(current_user.id)
-    return redirect(url_for('wishlist.wishlist'))
     
