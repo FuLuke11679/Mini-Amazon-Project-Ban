f66d0942a01b7ff197f41c22330e09d247f27bde
Max Murakami-Moses <mhm51@duke.edu>
2023-10-04 22:31:38 -0400

    part 3

--- app/wishlist.py
+++ app/wishlist.py
@@ -13,10 +13,10 @@ from flask import redirect, url_for
 from flask import Blueprint
 bp = Blueprint('wishlist', __name__)
 
-# from humanize import naturaltime
+from humanize import naturaltime
 
-# def humanize_time(dt):
-#     return naturaltime(datetime.datetime.now() - dt)
+def humanize_time(dt):
+    return naturaltime(datetime.datetime.now() - dt)
 
 
 @bp.route('/wishlist')
@@ -24,22 +24,20 @@ def wishlist():
     if (current_user.is_authenticated):
         items = WishlistItem.get_all(current_user.id)
         return jsonify([item.__dict__ for item in items])
-        # return render_template('wishlist.html',
-        #               items=items,
-        #               humanize_time=humanize_time)
-
-
+        return render_template('wishlist.html',
+                      items=items,
+                      humanize_time=humanize_time)
     else:
         return jsonify({}), 404 
 
 
-# @bp.route('/wishlist/add/<int:product_id>', methods=['POST'])
-# def wishlist_add(product_id):
-#         rows = app.db.execute("""
-# INSERT INTO Wishes(uid, pid, time_added)
-# VALUES(:uid, :pid, :time_added)
-# """,
-#                                   uid=current_user.id,
-#                                   pid=product_id,
-#                                   time_added=datetime.datetime.now())
-#         return redirect(url_for('wishlist.wishlist'))
+@bp.route('/wishlist/add/<int:product_id>', methods=['POST'])
+def wishlist_add(product_id):
+        rows = app.db.execute("""
+INSERT INTO Wishes(uid, pid, time_added)
+VALUES(:uid, :pid, :time_added)
+""",
+                                  uid=current_user.id,
+                                  pid=product_id,
+                                  time_added=datetime.datetime.now())
+        return redirect(url_for('wishlist.wishlist'))
