d62257b2b935304a77d4fea74beb74e3e0a68780
Damian Kim <djk57@duke.edu>
2023-12-08 19:33:32 -0500

    changes to topup

--- app/purchases.py
+++ app/purchases.py
@@ -3,7 +3,9 @@ from flask import render_template
 from flask_login import current_user
 import datetime
 
+
 from flask import current_app as app
+
 
 from .models.product import Product
 from .models.purchase import Purchase
@@ -11,10 +13,16 @@ from .models.wishlist import WishlistItem
 from flask import redirect, url_for
 from flask import request
 
+
 from flask import Blueprint
 bp = Blueprint('purchases', __name__)
 
+
 from humanize import naturaltime
+
+
+
+
 
 
 
@@ -32,48 +40,69 @@ import numpy as np
 
 
 
+
+
+
+
+
+
+
+
+
+
 def humanize_time(dt):
-    return naturaltime(datatime.datetime.now() - dt)
+   return naturaltime(datetime.datetime.now() - dt)
+
 
 def calculate_total_price(orderlist):
-    total_price = 0
-    for item in orderlist:
-        price = item.price_per_unit
-        total_price += item.quantity * price
-    return total_price
+   total_price = 0
+   for item in orderlist:
+       price = item.price_per_unit
+       total_price += item.quantity * price
+   return total_price
+
 
 @bp.route('/purchases/<int:user_id>/<int:page>', methods = ['GET'])
 def purchases(user_id, page):
-    purchasedItems = Purchase.get_all(user_id, page = page, per_page=20)
-    return render_template('purchases.html',
-                    purchasedItems=purchasedItems, 
-                    user_id = user_id, 
-                    page = page)
+   purchasedItems = Purchase.get_all(user_id, page = page, per_page=20)
+   return render_template('purchases.html',
+                   purchasedItems=purchasedItems,
+                   user_id = user_id,
+                   page = page)
+
 
 @bp.route('/mypurchases/<int:page>', methods = ['GET'])
 def mypurchases(page):
-    purchasedItems = Purchase.get_all(current_user.id, page = page, per_page=20)
+   purchasedItems = Purchase.get_all(current_user.id, page = page, per_page=20)
 
-    recommended_pids = recommend_based_on_purchase_history(current_user.id, user_item_matrix)
-    rec_products = Product.get_list_by_ids(recommended_pids)
-    return render_template('purchases.html',
-                    purchasedItems=purchasedItems, 
-                    rec_products = rec_products,
-                    user_id = current_user.id, 
-                    page = page)
-    
+
+   recommended_pids = recommend_based_on_purchase_history(current_user.id, user_item_matrix)
+   rec_products = Product.get_list_by_ids(recommended_pids)
+  
+   return render_template('purchases.html',
+                   purchasedItems=purchasedItems,
+                   rec_products = rec_products,
+                   user_id = current_user.id,
+                   page = page)
+  
 @bp.route('/purchases_search', methods=['GET', 'POST'])
 def purchases_search():
-    if request.method == 'POST':
-        user_id = request.form['user_id']
-        page = int(request.form.get('page', 1))
-        purchasedItems = Purchase.get_all(user_id, page)
-        return render_template('purchases.html',
-                      purchasedItems=purchasedItems, 
-                      user_id = user_id, 
-                      page = page) 
-    else:
-        return redirect(url_for('purchases.html'))
+   if request.method == 'POST':
+       user_id = request.form['user_id']
+       page = int(request.form.get('page', 1))
+       purchasedItems = Purchase.get_all(user_id, page)
+       return render_template('purchases.html',
+                     purchasedItems=purchasedItems,
+                     user_id = user_id,
+                     page = page)
+   else:
+       return redirect(url_for('purchases.html'))
+
+
+
+
+
+
 
 
 
@@ -87,38 +116,47 @@ column_names = ['id', 'uid', 'seller_id', 'pid', 'name', 'photo_url', 'tag', 'qu
 purchases_df.columns = column_names
 user_item_matrix = pd.pivot_table(purchases_df, values='quantity', index='uid', columns='pid', fill_value=0)
 
+
 def get_similar_users(user_id, matrix):
-    # Calculate cosine similarity between the user and all other users
-    similarities = cosine_similarity(matrix, [matrix.loc[user_id]])
-    # Sort users by similarity (excluding the user itself)
-    similar_users = np.argsort(similarities[:, 0])[::-1][1:]
-    return similar_users
+   # Calculate cosine similarity between the user and all other users
+   similarities = cosine_similarity(matrix, [matrix.loc[user_id]])
+   # Sort users by similarity (excluding the user itself)
+   similar_users = np.argsort(similarities[:, 0])[::-1][1:]
+   return similar_users
+
 
 def recommend_based_on_purchase_history(user_id, matrix, num_recommendations=5):
-    # Get similar users
-    similar_users = get_similar_users(user_id, matrix)
+   # Get similar users
+   similar_users = get_similar_users(user_id, matrix)
+   real_similar_users = [user for user in similar_users if user in matrix.index]
 
-    # Identify products that similar users have purchased but the target user has not
-    recommended_products = matrix.columns[
-        (matrix.loc[similar_users].sum(axis=0) > 0) & (matrix.loc[user_id] == 0)
-    ].tolist()
 
-    # Sort recommended products by the sum of purchases by similar users
-    recommended_products = sorted(
-        recommended_products,
-        key=lambda pid: matrix.loc[similar_users, pid].sum(),
-        reverse=True
-    )
 
-    return recommended_products[:num_recommendations]
+
+   # Identify products that similar users have purchased but the target user has not
+   recommended_products = matrix.columns[
+       (matrix.loc[real_similar_users].sum(axis=0) > 0) & (matrix.loc[user_id] == 0)
+   ].tolist()
+
+
+   # Sort recommended products by the sum of purchases by similar users
+   recommended_products = sorted(
+       recommended_products,
+       key=lambda pid: matrix.loc[real_similar_users, pid].sum(),
+       reverse=True
+   )
+
+
+   return recommended_products[:num_recommendations]
+
 
 @bp.route('/get_orders', methods=['GET', 'POST'])
 def get_orders():
-    user_id = current_user.id
-    orderlist = Purchase.get_all(user_id)  # Assuming you want to get purchases associated with the user
-    total_price = calculate_total_price(orderlist)
-    return render_template('orders.html',
-                           orderlist=orderlist,
-                           humanize_time=humanize_time,
-                           total_price=total_price,
-                           user_id = user_id)
+   user_id = current_user.id
+   orderlist = Purchase.get_all(user_id)  # Assuming you want to get purchases associated with the user
+   total_price = calculate_total_price(orderlist)
+   return render_template('orders.html',
+                          orderlist=orderlist,
+                          humanize_time=humanize_time,
+                          total_price=total_price,
+                          user_id = user_id)
--- app/users.py
+++ app/users.py
@@ -2,226 +2,321 @@ from flask import render_template, redirect, url_for, flash, request, get_flashe
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
-from humanize import naturaltime
-from .models.review import Review 
-from .models.sellerreview import SellerReview
+from wtforms import DateField, RadioField, FloatField, StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+
+
 from flask import current_app as app
 
-import datetime
-from humanize import naturaltime
+
 
 
 from wtforms.fields.simple import HiddenField
+
+
+
+
+
+
 
 
 from .models.user import User
 from .models.purchase import Purchase
 
 
+
+
+
+
+
+
 from flask import Blueprint
 bp = Blueprint('users', __name__)
 
+
 def humanize_time(dt):
-    return naturaltime(datetime.datetime.now() - dt)
+   return naturaltime(datetime.datetime.now() - dt)
+
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+  email = StringField('Email', validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  remember_me = BooleanField('Remember Me')
+  submit = SubmitField('Sign In')
+
+
+
+
+
+
 
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index.index'))
-        
-    get_flashed_messages()
-    form = LoginForm()
-    if form.validate_on_submit():
-        user, message = User.get_by_auth(form.email.data, form.password.data)
-        if user:
-            login_user(user)
-            next_page = request.args.get('next')
-            if not next_page or url_parse(next_page).netloc != '':
-                next_page = url_for('index.index')
-                return redirect(next_page)
+  if current_user.is_authenticated:
+      return redirect(url_for('index.index'))
+    
+  get_flashed_messages()
+  form = LoginForm()
+  if form.validate_on_submit():
+      user, message = User.get_by_auth(form.email.data, form.password.data)
+      if user:
+          login_user(user)
+          next_page = request.args.get('next')
+          if not next_page or url_parse(next_page).netloc != '':
+              next_page = url_for('index.index')
+              return redirect(next_page)
 
-        else:
-            flash(message, 'error')  
-            return redirect(url_for('users.login'))
 
-        
-    return render_template('login.html', title='Sign In', form=form)
+
+
+      else:
+          flash(message, 'error')
+          return redirect(url_for('users.login'))
+
+
+
+
+    
+  return render_template('login.html', title='Sign In', form=form)
+
+
+
+
+class TopUpForm(FlaskForm):
+  def validate_amount(form, field):
+     if field.data and float(field.data) < 0:
+         raise ValidationError('Amount must be non-negative!')
+  #only update with 0 or positive amounts
+
+
+  amount = FloatField('Amount', validators=[DataRequired()])
+  action = RadioField('Action', choices = [('deposit', 'Deposit'), ('withdraw', 'Withdraw')], validators=[DataRequired()])
+
+
+  credit_card_number = StringField('Credit Card Number', validators=[Length(min=15, max=16)])
+  cvv = StringField('CVV', validators=[Length(min=3, max=4), DataRequired()], )
+  expiry_date = DateField('Expiry Date', format='%Y-%m-%d', validators=[DataRequired()])
+
+
+
+
+  submit = SubmitField('Change balance')
+
+
+
+
+
+
+
 
 class UpdateInfoForm(FlaskForm):
-    def validate_balance(form, field):
-       if field.data and int(field.data) < 0:
-           raise ValidationError('Balance must be non-negative!')
-    #only update to balance of 0 or positive
 
-    def validate_email(self, email):
-        if email.data:
-            if email.data != current_user.email and User.email_exists(email.data):
-                raise ValidationError('Email is already in use. Choose a different one.')
 
-    firstname = StringField('First Name')
-    lastname = StringField('Last Name')
-    email = StringField('Email')
-    address = StringField('Address')
-    balance = StringField('Balance', validators = [validate_balance])
-    password = PasswordField('New Password', validators = [DataRequired()])
-    password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), 
-                                       EqualTo('password', 
-                                       message='Passwords must match')]
-    )
+  def validate_email(self, email):
+      if email.data:
+          if email.data != current_user.email and User.email_exists(email.data):
+              raise ValidationError('Email is already in use. Choose a different one.')
 
-    submit = SubmitField('Update Profile')
+
+
+
+  firstname = StringField('First Name')
+  lastname = StringField('Last Name')
+  email = StringField('Email')
+  address = StringField('Address')
+  password = PasswordField('New Password', validators = [DataRequired()])
+  password2 = PasswordField(
+      'Repeat Password', validators=[DataRequired(),
+                                     EqualTo('password',
+                                     message='Passwords must match')]
+  )
+
+
+
+
+  submit = SubmitField('Update Profile')
+
+
+
+
+
+
 
 
 class RegistrationForm(FlaskForm):
 
-    def validate_balance(form, field):
-       if field.data != '0':
-           raise ValidationError('Balance must be 0!')
-    #this is to make sure you can only register with a balance of 0
 
-    firstname = StringField('First Name', validators=[DataRequired()])
-    lastname = StringField('Last Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    address = StringField('Address', validators=[DataRequired()])
-    balance = StringField('Balance', validators=[DataRequired(), validate_balance])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(),
-                                       EqualTo('password',
-                                       message='Passwords must match')])
-    submit = SubmitField('Register')
 
-    def validate_email(self, email):
-        if User.email_exists(email.data):
-            raise ValidationError('Already a user with this email.')
+
+  def validate_balance(form, field):
+     if field.data != 0:
+         raise ValidationError('Balance must be 0!')
+  #this is to make sure you can only register with a balance of 0
+
+
+
+
+  firstname = StringField('First Name', validators=[DataRequired()])
+  lastname = StringField('Last Name', validators=[DataRequired()])
+  email = StringField('Email', validators=[DataRequired(), Email()])
+  address = StringField('Address', validators=[DataRequired()])
+  balance = FloatField('Balance', validators=[DataRequired(), validate_balance])
+  password = PasswordField('Password', validators=[DataRequired()])
+  password2 = PasswordField(
+      'Repeat Password', validators=[DataRequired(),
+                                     EqualTo('password',
+                                     message='Passwords must match')])
+  submit = SubmitField('Register')
+
+
+
+
+  def validate_email(self, email):
+      if User.email_exists(email.data):
+          raise ValidationError('Already a user with this email.')
+
+
+
+
+
+
 
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
-        form = UpdateInfoForm()
-        if form.validate_on_submit():
-        # Update user information in the database if not blank
-            email = form.email.data if form.email.data else current_user.email
-            password = form.password.data if form.password.data else current_user.password
-            firstname = form.firstname.data if form.firstname.data else current_user.firstname
-            lastname = form.lastname.data if form.lastname.data else current_user.lastname
-            address = form.address.data if form.address.data else current_user.address
-            balance = form.balance.data if form.balance.data else current_user.balance
+  if current_user.is_authenticated:
+      form = UpdateInfoForm()
+      if form.validate_on_submit():
+      # Update user information in the database if not blank
+          email = form.email.data if form.email.data else current_user.email
+          password = form.password.data if form.password.data else current_user.password
+          firstname = form.firstname.data if form.firstname.data else current_user.firstname
+          lastname = form.lastname.data if form.lastname.data else current_user.lastname
+          address = form.address.data if form.address.data else current_user.address
 
 
-            if User.update(current_user.id, #pass current userid as identifying element
-                        email,
-                        password,
-                        firstname,
-                        lastname,
-                        address,
-                        balance):
-                        
-                return redirect(url_for('index.index'))
 
-    else:
-        form = RegistrationForm()
-        if form.validate_on_submit():
-            if User.register(form.email.data,
-                            form.password.data,
-                            form.firstname.data,
-                            form.lastname.data,
-                            form.address.data,
-                            form.balance.data):
-                flash('Congratulations, you are now a registered user! Login to your profile!')
-                return redirect(url_for('users.login'))
-    return render_template('register.html', title='Register', form=form)
+
+          if User.update(current_user.id, #pass current userid as identifying element
+                      email,
+                      password,
+                      firstname,
+                      lastname,
+                      address):
+                    
+              return redirect(url_for('index.index'))
+
+
+
+
+  else:
+      form = RegistrationForm()
+      if form.validate_on_submit():
+          if User.register(form.email.data,
+                          form.password.data,
+                          form.firstname.data,
+                          form.lastname.data,
+                          form.address.data,
+                          form.balance.data):
+              flash('Congratulations, you are now a registered user! Login to your profile!')
+              return redirect(url_for('users.login'))
+  return render_template('register.html', title='Register', form=form)
+
+
+
+
+
+
+
+
+@bp.route('/top_up', methods=['GET', 'POST'])
+def top_up():
+  form = TopUpForm()
+  if form.validate_on_submit():
+      action = form.action.data
+      amount = form.amount.data
+
+
+      if action == 'deposit':
+           balance = current_user.balance + amount
+           if User.update_balance(current_user.id, balance):
+              return redirect(url_for('users.myprofile'))
+      elif action == 'withdraw':
+          if amount <= current_user.balance:
+               balance = current_user.balance - amount
+               User.update_balance(current_user.id, balance)
+               return redirect(url_for('users.myprofile'))  # Redirect to the user's profile page
+  
+          else:
+              return redirect(url_for('users.myprofile'))  # Redirect to the user's profile page
+  else:
+      return render_template('top_up.html', title='Topup', form=form)
+
+
+
+
 
 
 @bp.route('/logout')
 def logout():
-    logout_user()
-    return redirect(url_for('index.index'))
+  logout_user()
+  return redirect(url_for('index.index'))
+
+
+
 
 @bp.route('/myprofile')
 def myprofile():
-    if current_user.is_authenticated:
-        reviews = Review.get_all_by_uid_since(current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
-        sellerReviews = SellerReview.get_all_by_uid_since(current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
-    else:
-        reviews = None
-        sellerReviews = None
+ return render_template('myprofile.html')
 
-    # print(reviews)
 
-    return render_template('myprofile.html', 
-                        reviews=reviews,
-                        sellerReviews=sellerReviews,
-                        humanize_time=humanize_time)
 
 
 @bp.route('/publicprofile/<int:user_id>', methods = ['GET'])
 def publicprofile(user_id):
-    #user_id = request.form['user_id']
-    seller = is_seller(user_id)
-    userInfo = User.get(user_id)
-    
-    average_rating_a = SellerReview.total_average(user_id)
-    if average_rating_a == None:
-        average_rating = None
-    else:
-        average_rating = average_rating_a[0]
-    
-    reviews_for_this_seller = SellerReview.reviews_for_this_seller(user_id)
-    num_of_reviews = len(reviews_for_this_seller or "")
+  #user_id = request.form['user_id']
+  seller = is_seller(user_id)
+  userInfo = User.get(user_id)
+  return render_template('publicprofile.html',
+                  user_id = user_id,
+                  userInfo = userInfo,
+                  seller = seller)
 
 
-    return render_template('publicprofile.html',
-                    user_id = user_id, 
-                    userInfo = userInfo, 
-                    seller = seller,
-                    reviews_for_this_seller=reviews_for_this_seller,
-                    humanize_time=humanize_time,
-                    average_rating = average_rating,
-                    num_of_reviews=num_of_reviews)
+
 
 @bp.route('/user_search', methods=['GET', 'POST'])
 def user_search():
-    if request.method == 'POST':
-        user_id = request.form['user_id']
-        seller = is_seller(user_id)
-        userInfo = User.get(user_id)
+  if request.method == 'POST':
+      user_id = request.form['user_id']
+      seller = is_seller(user_id)
+      userInfo = User.get(user_id)
 
-        all_seller_reviews = SellerReview.reviews_for_this_seller(user_id)
-        num_of_reviews = len(all_seller_reviews or "")
 
-        average_rating_a = SellerReview.total_average(user_id)
-        if average_rating_a == None:
-            average_rating = None
-        else:
-            average_rating = average_rating_a[0]
 
-        return render_template('publicprofile.html',
-                    user_id = user_id, 
-                    userInfo = userInfo, 
-                    seller = seller,
-                    humanize_time=humanize_time,
-                    num_of_reviews = num_of_reviews,
-                    average_rating = average_rating)
-    else:
-        return redirect(url_for('index.html'))
+
+      return render_template('publicprofile.html',
+                  user_id = user_id,
+                  userInfo = userInfo,
+                  seller = seller)
+  else:
+      return redirect(url_for('index.html'))
+
+
+
+
+
+
 
 
 def is_seller(user_id):
-    rows = app.db.execute('''
-        SELECT id FROM Sellers
-        WHERE uid = :user_id
-    ''', user_id=user_id)
+  rows = app.db.execute('''
+      SELECT id FROM Sellers
+      WHERE uid = :user_id
+  ''', user_id=user_id)
 
-    return bool(rows)
+
+
+
+  return bool(rows)
