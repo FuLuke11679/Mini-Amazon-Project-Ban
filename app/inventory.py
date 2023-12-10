b73504980716c875c06cb546cb982ee16c15d8ff
Jason Zhao <jwz10@duke.edu>
2023-12-09 19:03:14 -0500

    inventory comments

--- app/inventory.py
+++ app/inventory.py
@@ -16,8 +16,10 @@ bp = Blueprint('inventory', __name__)
 
 from flask import redirect, url_for
 
+#this page shows the current user inventory
 @bp.route('/inventory', methods=['GET', 'POST'])
 def inv():
+    #redirects users back to home page if they are not a seller
     if(InventoryItem.get_seller(current_user.id)[0] == 0):
         return redirect(url_for('index.index'))
     itemsInInventory = InventoryItem.get_all_by_uid_since(
@@ -26,6 +28,7 @@ def inv():
         items=itemsInInventory,
         humanize_time=humanize_time)
 
+#adds a product into a seller's inventory
 @bp.route('/inventory/add', methods=['POST'])
 def inventory_add():
     product_id = request.form['product_id']
@@ -39,19 +42,23 @@ VALUES(:uid, :pid, :time_added, 1)
                                   pid=product_id,
                                   time_added=datetime.datetime.now())
             return redirect(url_for('inventory.inv'))
+        #error catching
         except Exception as e:
             print(str(e))
             return jsonify({}), 404
     else:
         return jsonify({}), 404
 
+#defining time
 def humanize_time(dt):
     return naturaltime(datetime.datetime.now() - dt)
 
+#this page only shows a given seller's inventory, without the ability to change it
 @bp.route('/inventory/view', methods=['GET','POST'])
 def inventory_view():
     if request.method == 'POST':
         user_id = request.form['user_id']
+        #redirects users back to home page if input is not a valid seller id
         if(InventoryItem.get_seller(user_id)[0] == 0):
             return redirect(url_for('index.index'))
         itemsInInventoryView = InventoryItem.get_all_by_uid_since(
@@ -61,6 +68,7 @@ def inventory_view():
             humanize_time=humanize_time)
     return('index.html')
 
+#deletes a product from a seller's inventory
 @bp.route('/inventory/delete/<int:product_id>', methods=['POST'])
 def inventory_delete(product_id):
     if current_user.is_authenticated:
@@ -78,7 +86,8 @@ AND pid = :pid
             return jsonify({}), 404
     else:
         return jsonify({}), 404
-    
+
+#updates the quantity of a specific item in a seller's inventory  
 @bp.route('/inventory/update/<int:product_id>', methods=['POST'])
 def inventory_update(product_id):
     new_quantity = request.form['quantity']
@@ -100,11 +109,14 @@ AND pid = :pid
     else:
         return jsonify({}), 404
 
+#register a user as a seller by inserting their uid into the seller's database
 @bp.route('/inventory/register', methods=['GET', 'POST'])
 def inventory_register():
+    #redirects user back to home page if they are already a seller
     if(InventoryItem.get_seller(current_user.id)[0] == 1):
         return redirect(url_for('index.index'))
     if current_user.is_authenticated:
+        #finds highest id in sellers so that the next uid can be successfully inserted into the table
         numSellers = InventoryItem.get_num()[0]
         try:
             rows = app.db.execute("""
@@ -120,6 +132,7 @@ VALUES(:id, :uid)
     else:
         return jsonify({}), 404
     
+#shows sellers the orders that have been sent by users for fulfillment
 @bp.route('/inventory/order_fulfill', methods=['GET', 'POST'])
 def inventory_order_fulfill():
     if current_user.is_authenticated:
@@ -131,7 +144,8 @@ def inventory_order_fulfill():
                            user_id=user_id)
     else:
         return jsonify({}), 404
-    
+
+#allows sellers to fulfill orders that are in progress   
 @bp.route('/inventory/confirm_fulfill/<int:order_id>/<int:product_id>', methods=['GET', 'POST'])
 def inventory_confirm_fulfill(order_id, product_id):
     if current_user.is_authenticated:
--- app/models/inventory.py
+++ app/models/inventory.py
@@ -1,6 +1,7 @@
 from flask import current_app as app
 
 
+#initializes inventory item
 class InventoryItem:
     def __init__(self, id, uid, pid, time_added, quantity):
         self.id = id
@@ -9,6 +10,7 @@ class InventoryItem:
         self.time_added = time_added
         self.quantity = quantity
 
+#gets an inventory item given the product's id
     @staticmethod
     def get(id):
         rows = app.db.execute('''
@@ -19,6 +21,7 @@ WHERE id = :id
                               id=id)
         return InventoryItem(*(rows[0])) if rows else None
 
+#gets the inventory of a seller given their user id
     @staticmethod
     def get_all_by_uid_since(uid, since):
         rows = app.db.execute('''
@@ -31,7 +34,8 @@ ORDER BY time_added DESC
                               uid=uid,
                               since=since)
         return [InventoryItem(*row) for row in rows]
-    
+
+#returns 0 if the given uid is not a seller, 1 otherwise
     @staticmethod
     def get_seller(uid):
         num = app.db.execute('''
@@ -42,6 +46,7 @@ WHERE uid = :uid
                               uid=uid)
         return (num[0])
     
+#gets highest id in sellers
     @staticmethod
     def get_num():
         num = app.db.execute('''
